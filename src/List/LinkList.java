d6857c3fcbbab1c93250dd84ba93ab0d73addf03
Willokkkkkk <173891069@qq.com>
2017-07-18 17:46:16 +0800

    创建 单向链表

---
+++ src/List/LinkList.java
@@ -0,0 +1,85 @@
+package List;
+
+/**
+ * Created by panchaoliang on 2017/7/18.
+ * data structure
+ * List
+ * 2017/07/18
+ */
+class Node {
+    private Object data;
+    private Node nextNode;
+
+    public Node() {
+    }
+
+    public Node(Object data) {
+        this.data = data;
+        this.nextNode = null;
+    }
+}
+
+/**
+ * 带头结点的链式链表,下标从0开始;
+ * @author Administrator
+ *
+ */
+public class LinkList implements List {
+    Node headNode;  //头结点
+    int size;   //链表的大小
+
+    public LinkList() {
+        this.headNode = new Node();
+        size = 0;
+    }
+
+    @Override
+    public void add(Object e) {
+
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public Object get(int i) {
+        return null;
+    }
+
+    @Override
+    public int indexOf(Object e) {
+        return 0;
+    }
+
+    @Override
+    public void insert(int i, Object e) {
+
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public int lastIndexOf(Object e) {
+        return 0;
+    }
+
+    @Override
+    public void remove(int i) {
+
+    }
+
+    @Override
+    public void set(int i, Object e) {
+
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+}
