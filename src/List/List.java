9fc34c7f2863b42891cb17589cf7d9ebab16cde5
Willokkkkkk <173891069@qq.com>
2017-07-18 17:23:50 +0800

    完成顺序表

--- src/List/List.java
+++ src/List/List.java
@@ -28,20 +28,20 @@ package List;
      void clear();
     /**
      * 获取i位置的元素
-     *  i
+     *
      * 
      */
      Object get(int i);
     /**
      * 返回列表中首次出现指定元素的索引，如果列表不包含此元素，则返回 -1。
-     *  e
+     *
      * 
      */
      int indexOf(Object e);
     /**
      * 在i后面插入一个元素e
-     *  i
-     *  e
+     *
+     *
      */
      void insert(int i, Object e);
     /**
@@ -51,19 +51,19 @@ package List;
      boolean isEmpty();
     /**
      * 回列表中最后出现指定元素的索引，如果列表不包含此元素，则返回 -1。
-     *  e
+     *
      * 
      */
      int lastIndexOf(Object e);
     /**
      *  移除列表中指定位置的元素
-     *  i
+     *
      */
      void remove(int i);
     /**
      * 用指定元素替换列表中指定位置的元素（可选操作）。
-     *  i
-     *  e
+     *
+     *
      */
      void set(int i, Object e);
     /**
