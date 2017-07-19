c79ab497de1e34f5fb3dcca5306d9c57c9c97f64
Willokkkkkk <173891069@qq.com>
2017-07-19 16:57:00 +0800

    完善

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # DataStructure
 自己参考资料写的数据结构.
 可能存在很多问题 ,请多多指教
+参考网站  http://blog.csdn.net/luoweifu/article/details/8507835
---
+++ src/queue/CircularQueue.java
@@ -0,0 +1,60 @@
+package queue;
+
+import java.io.Serializable;
+
+/**
+ * Created by panchaoliang on 2017/7/19.
+ * data structure
+ * queue
+ * 顺序循环队列
+ * 为充分利用向量空间，克服"假上溢"现象的方法是：将向量空间想象为一个首尾相接的圆环，
+ * 并称这种向量为循环向量。存储在其中的队列称为循环队列（Circular Queue）。
+ *
+ * 循环队列中进行出队、入队操作时，头尾指针仍要加1，
+ * 朝前移动。只不过当头尾指针指向向量上界（QueueSize-1）时，其加1操作的结果是指向向量的下界0。
+ * 2017/07/19
+ */
+public class CircularQueue implements Queue,Serializable {
+    private static  int  DEFAULT_SIZE = 10;
+    private Object[] array = null;
+    private int front, rear, size; //队首，队尾标注和队列的大小
+
+    public CircularQueue() {
+        array = new Object[DEFAULT_SIZE];
+        front=rear=size= 0;
+    }
+
+    @Override
+    public void clear() {
+        for(int i=0; i<DEFAULT_SIZE; i++) {
+            array[i] = null;
+        }
+        front=rear=size= 0;
+    }
+
+    @Override
+    public Object deQueue() {
+        return null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return rear==front && size ==0;
+    }
+
+    @Override
+    public Object peek() {
+        return null;
+    }
+
+    @Override
+    public void push(Object obj) {
+        size++;
+        front++;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+}
