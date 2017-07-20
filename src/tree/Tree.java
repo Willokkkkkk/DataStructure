4ab9bc16c86d51be9768c5db5529422642e0de79
Willokkkkkk <173891069@qq.com>
2017-07-20 17:45:11 +0800

    tree  1st commit

---
+++ src/tree/Tree.java
@@ -0,0 +1,12 @@
+package tree;
+
+/**
+ * Created by panchaoliang on 2017/7/20.
+ * data structure
+ * tree
+ * 2017/07/20
+ * 线性表是一头一尾 , 典型的一对一
+ * 树，非线性结构的典型例子，不再是一对一，而变成了一对多（而图则可以是 多对多）
+ */
+public class Tree {
+}
